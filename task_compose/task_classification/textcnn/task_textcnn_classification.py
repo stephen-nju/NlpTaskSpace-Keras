897936383ca5537026a76e818a991306a16282a2
zhubin <zhubinnju@outlook.com>
2023-05-23 18:20:49 +0800

    Update

--- task_compose/task_classification/textcnn/task_textcnn_classification.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@author: zhubin
-@email:18073701@cnsuning.com
-@license: Apache Licence
-@software:PyCharm
-@file: task_textcnn_classification.py
-@time: 2021/9/10 9:50
-"""
